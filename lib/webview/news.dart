ea4082ac0235658bc68fbbb6831420eb2570cd9b
Olayode Enoch <olayodeenoch@gmail.com>
2021-07-24 13:00:04 +0100

    Created news, sports, and politics web pages

--- lib/webview/news.dart
+++ lib/webview/news.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
 import 'package:flutter/services.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:starnews/admob_service.dart';
 import '../drawer/drawer.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class StarnewsNews extends StatefulWidget {
   const StarnewsNews({Key? key}) : super(key: key);
@@ -10,7 +14,15 @@ class StarnewsNews extends StatefulWidget {
 }
 
 class _StarnewsNewsState extends State<StarnewsNews> {
-  final _flutterwebview = FlutterWebviewPlugin();
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +30,7 @@ class _StarnewsNewsState extends State<StarnewsNews> {
         backgroundColor: Color(0xff4f0034),
         brightness: Brightness.light, // status bar brightness
         backwardsCompatibility: false,
-        systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.white10),
+        systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.black12),
         title: Text('News',
           style: TextStyle(
               fontFamily: "Montserrat Medium"
@@ -26,23 +38,59 @@ class _StarnewsNewsState extends State<StarnewsNews> {
         centerTitle: true,
         elevation: 0,
       ),
-      drawer: MainDrawer(),
 
+      drawer: MainDrawer(),
+      // key.openEndDrawer(),
       body: Builder(builder: (BuildContext context) {
-        return WebviewScaffold(
-          url: 'https://www.starnews.com.ng/category/news/',
-          withZoom: true,
-          scrollBar: true,
-          withLocalStorage: true,
-          withJavascript: true,
-          initialChild: Center(child: Text('Loading...')),
+        return WebView(
+          initialUrl: 'https://starnews.com.ng/category/news/',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          onProgress: (int progress) {
+            print("Loading (progress : $progress%)");
+          },
+          javascriptChannels: <JavascriptChannel>{
+            _toasterJavascriptChannel(context),
+          },
+          navigationDelegate: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.starnews.com.ng/category/news/')) {
+              print('blocking navigation to $request}');
+              // return NavigationDecision.prevent;
+            }
+            print('allowing navigation to $request');
+            return NavigationDecision.navigate;
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          gestureNavigationEnabled: true,
         );
-      }
-      ),);
+      }),
+
+      bottomNavigationBar: Container(
+        height: 50,
+        child: AdWidget(
+          key: UniqueKey(),
+          ad: AdMobService.createBannerAd()..load(),
+        ),
+      ),
+      backgroundColor: Colors.grey[200],
+    );
   }
-  @override
-  void dispose() {
-    _flutterwebview.dispose();
-    super.dispose();
-  }
+}
+
+JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+  return JavascriptChannel(
+      name: 'Toaster',
+      onMessageReceived: (JavascriptMessage message) {
+        // ignore: deprecated_member_use
+        Scaffold.of(context).showSnackBar(
+          SnackBar(content: Text(message.message)),
+        );
+      });
 }
--- lib/webview/sports.dart
+++ lib/webview/sports.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
 import 'package:flutter/services.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:starnews/admob_service.dart';
+import '../drawer/drawer.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class StarnewsSports extends StatefulWidget {
   const StarnewsSports({Key? key}) : super(key: key);
@@ -9,16 +14,23 @@ class StarnewsSports extends StatefulWidget {
 }
 
 class _StarnewsSportsState extends State<StarnewsSports> {
-  final _flutterwebview = FlutterWebviewPlugin();
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-      url: 'https://www.starnews.com.ng/category/sports/',
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xff4f0034),
         brightness: Brightness.light, // status bar brightness
         backwardsCompatibility: false,
-        systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.white10),
+        systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.black12),
         title: Text('Sports',
           style: TextStyle(
               fontFamily: "Montserrat Medium"
@@ -26,20 +38,59 @@ class _StarnewsSportsState extends State<StarnewsSports> {
         centerTitle: true,
         elevation: 0,
       ),
-      withZoom: true,
-      scrollBar: true,
-      withLocalStorage: true,
-      withJavascript: true,
-      initialChild: Center (child: Text('Loading...')),
-      // bottomNavigationBar: Padding(
-      //   padding: EdgeInsets.all(12),
-      //   child: Text('Bottom Navigation'),
-      // ),
+
+      drawer: MainDrawer(),
+      // key.openEndDrawer(),
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: 'https://starnews.com.ng/category/sports/',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          onProgress: (int progress) {
+            print("Loading (progress : $progress%)");
+          },
+          javascriptChannels: <JavascriptChannel>{
+            _toasterJavascriptChannel(context),
+          },
+          navigationDelegate: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.starnews.com.ng/category/sports/')) {
+              print('blocking navigation to $request}');
+              // return NavigationDecision.prevent;
+            }
+            print('allowing navigation to $request');
+            return NavigationDecision.navigate;
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          gestureNavigationEnabled: true,
+        );
+      }),
+
+      bottomNavigationBar: Container(
+        height: 50,
+        child: AdWidget(
+          key: UniqueKey(),
+          ad: AdMobService.createBannerAd()..load(),
+        ),
+      ),
+      backgroundColor: Colors.grey[200],
     );
   }
-  @override
-  void dispose() {
-    _flutterwebview.dispose();
-    super.dispose();
-  }
+}
+
+JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+  return JavascriptChannel(
+      name: 'Toaster',
+      onMessageReceived: (JavascriptMessage message) {
+        // ignore: deprecated_member_use
+        Scaffold.of(context).showSnackBar(
+          SnackBar(content: Text(message.message)),
+        );
+      });
 }
