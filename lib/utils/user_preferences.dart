27b263198a8b0c5f66997863c22927879156c018
Olayode Enoch <olayodeenoch@gmail.com>
2021-09-01 20:11:55 +0100

    Updated about the app

--- lib/utils/user_preferences.dart
+++ lib/utils/user_preferences.dart
@@ -8,7 +8,16 @@ class UserPreferences {
     email:
       'info@starnews.com.ng',
     about:
-      'About the app will be here. About the app will be here. About the app will be here. About the app will be here. About the app will be here. About the app will be here. About the app will be here. About the app will be here. About the app will be here',
+      'Starnews is a media outlet that delivers relevant up-to-date news; we serve as a '
+          'bridge to the information gap, ensuring that people get informed about happenings '
+          'around both locally and internationally. With more than 50,000 active subscribers, '
+          'and over 500,000 followers on our various social media platforms, we deliver our media '
+          'content to a wide and diverse group of people in all the 36 states of Nigeria.'
+          '\n \nWe meet the expectation of our readers and serve up relevant and well-targeted '
+          'newsletters to them. By this, we do not only share information about happenings '
+          'and up-to-date news, but we also share information about other businesses too, and '
+          'report political activities of politicians to ensure their businesses and performance '
+          'can leverage on the wide reach audience we enjoy.',
     isDarkMode: false,
   );
 }
